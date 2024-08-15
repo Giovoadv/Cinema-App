e8979543e190e333334a38eb285ba0bafdef45fe
giovo_adv@hotmail.com <giovo_adv@hotmail.com>
2024-08-14 21:53:03 -0500

    Fixing Images

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -11,35 +11,35 @@ const Carousel = () => {
       <div className="carousel-inner" id="carouselId">
         <div className="carousel-item active">
           <img
-            src="/src/assets/MoviesImages/Deadpool&Wolverine.jpg"
+            src="src/assets/MoviesImages/Deadpool&Wolverine.jpg"
             className="d-block w-100"
             alt="..."
           />
         </div>
         <div className="carousel-item">
           <img
-            src="/src/assets/MoviesImages/romulus-1080x36040.webp"
+            src="src/assets/MoviesImages/romulus-1080x36040.webp"
             className="d-block w-100"
             alt="..."
           />
         </div>
         <div className="carousel-item">
           <img
-            src="/src/assets/MoviesImages/Terror.jpg"
+            src="src/assets/MoviesImages/Terror.jpg"
             className="d-block w-100"
             alt="..."
           />
         </div>
         <div className="carousel-item">
           <img
-            src="/src/assets/MoviesImages/iewu_1080x360_er_banners_v2_aug.webp"
+            src="src/assets/MoviesImages/iewu_1080x360_er_banners_v2_aug.webp"
             className="d-block w-100"
             alt="..."
           />
         </div>
         <div className="carousel-item">
           <img
-            src="/src/assets/MoviesImages/btlbtl_cinemark_dated_1080x360.jpg"
+            src="src/assets/MoviesImages/btlbtl_cinemark_dated_1080x360.jpg"
             className="d-block w-100"
             alt="..."
           />
